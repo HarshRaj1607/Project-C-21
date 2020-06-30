f9a80db569ad8230a17ac9f21ead91665a9e89af
HarshRaj1607 <63408882+HarshRaj1607@users.noreply.github.com>
2020-06-30 14:03:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,51 +12,41 @@ var bullet,speed,weight;
 
 function setup(){
   createCanvas(1600,400);
+   
+  bullet = createSprite(50,200,50,10);
+  wall = createSprite(1200,200,60,height/2);
+  thicnkness = random(22,83);
+  speed = random(223,321); 
+  weight = random(30,52); 
+} 
+
+function draw() { 
+  background(0,0,0); 
+  bullet.shapeColor = "white"; 
+  bullet.velocityX = speed; 
+  wall.shapeColor = color(80,80,80); 
   
-  thickness = random(22,83);
-  wall = createSprite(1400,200,thickness,height/2);
-  wall.shapeColor = color(80,80,80);
-
-  bullet = createSprite(200,200,75,35);
-  speed = random(223,321);
-  bullet.velocityX = speed;
-  weight = random(30,52);
-  bullet.weight = weight;
-  bullet.shapeColor = color(255,255,255);
   
-}
+  function hasCollided(bullet, wall){
+     bulletRightEdge = bullet.x + bullet.width; 
+     wallLeftEdge = wall.x; 
+     if ( bulletRightEdge>= wallLeftEdge) { 
+       return true } 
+       return false; } 
 
-
-function draw(){
-background(0,0,0)
-  //sphere.x = World.mouseX;
-  //sphere.y = World.mouseY;
- 
- if(hasCollided(bullet,wall)) {
-  bullet.velocityX = 0;
-  var damage = weight * speed * speed/ (thickness * thickness * thickness)
- 
- 
-
-  if(damage > 10)
-  {
-    wall.shapeColor = color(250,0,0);
-  }
+     if (hasCollided(bullet,wall)) { 
+       bullet.velocityX = 0; 
+       var damage = 0.5* speed* speed* weight/(thickness*thickness*thickness); 
+       if (damage> 10) { 
+         wall.shapeColor = "red"; 
+        } 
+        if (damage<10 ) { 
+          wall.shapeColor = "green"; 
+        } 
+      } drawSprites(); 
+    }
   
-  if(damage < 10){
-    wall.shapeColor = color(0,250,0);
-  }
-}
 
 
-drawSprites ();
-}
 
-function hasCollided(bullet,wall){
-bulletRightEdge = bullet.x + bullet.widht;
-wallLeftEdge = wall.x;
-if(bulletRightEdge >= wallLeftEdge){
-  return  true;
-}
-return false;
-}
+
